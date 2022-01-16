ce923f645c2f3bdfe2d533d122b7f44921f414ec
PrinceSP <princepasombaran@gmail.com>
2022-01-16 21:31:41 +0800

    make new notes component to show some latest notes

--- src/components/molecules/newNotesInfo/index.js
+++ src/components/molecules/newNotesInfo/index.js
@@ -1,15 +1,25 @@
 import React from 'react'
+import "./newnotes.scss"
 
 const NewNotes = (props) => {
   return (
-    <div>
+    <div className="newNote-container">
       <h3>Nota Terbaru</h3>
       <table>
-        <tr>
+        <tr className="table-header">
           <th>Pengguna</th>
           <th>Tanggal & Waktu</th>
           <th>ID Nota</th>
           <th>Status</th>
+        </tr>
+        <tr>
+          <td className='username'>
+            <div className="profileIcon"/>
+            <p>Dinda Gledis Mamahit</p>
+          </td>
+          <td className='blurText'>23 mei 2021</td>
+          <td className='blurText'>89774654</td>
+          <td>Lunas</td>
         </tr>
       </table>
     </div>
