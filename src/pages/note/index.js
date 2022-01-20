df642fb3f53be9ac83eb8a3d0ba92647d5117e8d
PrinceSP <princepasombaran@gmail.com>
2022-01-20 10:54:25 +0800

    modified search box in note page

--- src/pages/note/index.js
+++ src/pages/note/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import {TopBar,SideBar,Button} from '../../components'
 import "./note.scss"
-import SearchIcon from '@mui/icons-material/Search';
+// import SearchIcon from '@mui/icons-material/Search';
 
 const NotePage = (props) => {
-  const Search = ()=>{
-    <SearchIcon/>
+  // const Search = ()=>{
+  //   <SearchIcon/>
+  // }
+  const modalToggle=()=>{
+    alert('it works')
   }
   return (
     <div className="main-container">
@@ -18,13 +21,14 @@ const NotePage = (props) => {
               <p style={{fontSize:'1.5em',fontWeight:400}}>Nota-nota</p>
               <p style={{fontSize:'1em',fontWeight:300}}>Daftar semua nota dan transaksi</p>
             </div>
-            <Button name="+Nota Baru"/>
+            <Button name="+Nota Baru" onClick={modalToggle}/>
           </div>
           <form>
+            {/**<SearchIcon style={{position:'absolute',left:'15.5rem',marginTop:8}}/>**/}
             <input
               type="text"
               id="header-search"
-              placeholder={`${<SearchIcon/>} Search blog posts`}
+              placeholder={`Search blog posts`}
               name="search"/>
           </form>
         </div>
