36d1c6443694bcd48596270e001f274f4885bee9
PrinceSP <princepasombaran@gmail.com>
2022-01-21 17:54:32 +0800

    modified duplicate key border on input component

--- src/components/atoms/input/index.js
+++ src/components/atoms/input/index.js
@@ -4,7 +4,7 @@ const Input = ({label,name,holder,width,type,height}) => {
   return (
     <div style={{display:'flex',flexDirection:'column',justifyContent:'space-between'}}>
       <label>{label}</label>
-      <input type={type} style={{height,width,border:"1px ",border:"1px solid #9DD6E7"}} name={name} placeholder={holder}/>
+      <input type={type} style={{height,width,border:"1px solid #9DD6E7"}} name={name} placeholder={holder}/>
     </div>
   )
 }
