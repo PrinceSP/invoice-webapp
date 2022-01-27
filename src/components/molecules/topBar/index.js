15f78bdd35db5e75f322dc7090ac4729842d0077
PrinceSP <princepasombaran@gmail.com>
2022-01-27 13:12:22 +0800

    use netilify token

--- src/components/molecules/topBar/index.js
+++ src/components/molecules/topBar/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import './topBar.scss'
 import PersonIcon from '@mui/icons-material/Person';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
@@ -9,7 +10,9 @@ const TopBar = (props) => {
     <div className="topbar-container">
       <div className="topbar-wrapper">
         <div>
-          <span className="logo">Invoice Admin</span>
+          <Link style={{textDecoration:'none'}} to="/">
+            <span className="logo">Invoice Admin</span>
+          </Link>
         </div>
         <div className="rightComponent">
           <NotificationsNoneIcon/>
