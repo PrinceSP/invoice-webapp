3fe56784b39186470f67c08ba25e6972f9c6e5b6
PrinceSP <princepasombaran@gmail.com>
2022-02-18 12:49:04 +0800

    remove log in localise date

--- src/configs/localiseDate.js
+++ src/configs/localiseDate.js
@@ -6,8 +6,6 @@ const localizeDateStr = (actualDate)=>{
   let fullDate = actualDate.split('T').shift()
   let [day, month, year] = fullDate.split('-')
 
-  console.log(actualDate);
-
   return actualDate = `${day} ${months[month-1]} ${year}`;
 }
 
