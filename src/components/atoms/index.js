cb6cd72c58afe0aae2e0440f500d658137143004
PrinceSP <princepasombaran@gmail.com>
2022-02-18 09:12:13 +0800

    export new atoms and molecules

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -2,5 +2,6 @@ import Button from './button'
 import Input from './input'
 import Gap from './gap'
 import CheckBox from './checkbox'
+import StyledDot from './styledDot'
 
-export {Button,Input,Gap,CheckBox}
+export {Button,Input,Gap,CheckBox,StyledDot}
--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -5,5 +5,6 @@ import NewNotes from './newNotesInfo'
 import DatasGrid from './dataGrid'
 import Table from './table'
 import Footer from './footer'
+import DetailsNote from './detailsNote'
 
-export {TopBar,SideBar,Chart,NewNotes,DatasGrid,Table,Footer}
+export {TopBar,SideBar,Chart,NewNotes,DatasGrid,Table,Footer,DetailsNote}
